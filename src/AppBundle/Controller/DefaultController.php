3bc1e19b1fb1e6d3571de4725194bc4ea5746173
Mehyo <zibo09@gmail.com>
2015-09-17 16:09:43 +0200

    Add blog base
    Add blog base function Write, Read, and homepage actualisation.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,15 +9,16 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
+
+
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+      $response = $this->forward('AppBundle:Blog:recentArticles');
+        return $response;
     }
 
     /**
@@ -39,4 +40,14 @@ class DefaultController extends Controller
           'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
       ));
     }
+
+    /**
+    * @Route("/blog", name="blog")
+    */
+    public function blogAction(Request $request)
+    {
+      $twig->addExtension(new Twig_Extensions_Extension_Text());
+      return $this->redirect($this->generateUrl('post_new'));
+    }
+
 }
