3acebe2fe45d09de594a35db7fa070a55056e11d
Nicolas Endredi <nicolas.endredi@gmail.com>
2015-11-17 02:40:17 +0100

    [Tidying up] Made the Constraint "prettier"

--- src/AppBundle/Validator/Constraints/HasDifferentRolesValidator.php
+++ src/AppBundle/Validator/Constraints/HasDifferentRolesValidator.php
@@ -10,19 +10,35 @@ class HasDifferentRolesValidator extends ConstraintValidator
   public function validate($object, Constraint $constraint)
   {
     if ($object->getRole1() === $object->getRole2() OR
-          $object->getRole1() === $object->getRole3() OR
-          $object->getRole1() === $object->getRole4() OR
-          $object->getRole1() === $object->getRole5() OR
-          $object->getRole2() === $object->getRole3() OR
-          $object->getRole2() === $object->getRole4() OR
-          $object->getRole2() === $object->getRole5() OR
-          $object->getRole3() === $object->getRole4() OR
-          $object->getRole3() === $object->getRole5() OR
-          $object->getRole4() === $object->getRole5()
-    ) {
+    $object->getRole1() === $object->getRole3() OR
+    $object->getRole1() === $object->getRole4() OR
+    $object->getRole1() === $object->getRole5()){
       $this->context->buildViolation($constraint->message)
       ->atPath('role_1')
       ->addViolation();
+      return;
+    }
+
+    if ($object->getRole2() === $object->getRole3() OR
+    $object->getRole2() === $object->getRole4() OR
+    $object->getRole2() === $object->getRole5()){
+      $this->context->buildViolation($constraint->message)
+      ->atPath('role_2')
+      ->addViolation();
+      return;
+    }
+    if ($object->getRole3() === $object->getRole4() OR
+    $object->getRole3() === $object->getRole5()){
+      $this->context->buildViolation($constraint->message)
+      ->atPath('role_3')
+      ->addViolation();
+      return;
+    }
+    if ($object->getRole4() === $object->getRole5()){
+      $this->context->buildViolation($constraint->message)
+      ->atPath('role_4')
+      ->addViolation();
+      return;
     }
   }
-  }
+}
